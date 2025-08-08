8168adb60c8481b99b7f1945e000c465407ff88e
sky <kalininskyvorobei2002@mail.ru>
2025-08-08 18:42:02 +0700

    Update lesson_2_task_2.py

--- lesson_2_task_2.py
+++ lesson_2_task_2.py
@@ -1,6 +1,7 @@
 def is_year_leap(year):
   return year % 4 == 0
-  year_to_check = 2020
-  result = is_year_leap(year_to_chech)
+  year_to_check = [2024, 2023, 2000, 1900, 2020]
+  for year in years_to_check:
+    result = is_year_leap(year)
 
-print(f"год {year_to_check}: {result}")
+print(f"год {year}: {result}")
