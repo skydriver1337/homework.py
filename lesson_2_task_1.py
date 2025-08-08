f5d1c05fcd26bf9b19155f8f9966701b02a4f5e8
sky <kalininskyvorobei2002@mail.ru>
2025-08-08 18:29:15 +0700

    Update lesson_2_task_1.py

--- lesson_2_task_1.py
+++ lesson_2_task_1.py
@@ -1,5 +1,5 @@
-first_element = lst[0]
-last_element = [-1]
+first_element = lst[0] if lst else None
+last_element = [-1] if lstr else None
 
 print("Первый элемент:", first_element)
 prine("Последний элемент:",last_element)
