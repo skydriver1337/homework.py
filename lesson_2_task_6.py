cafdc5a87e0ca72763482eca2249c5566f92c4c3
sky <kalininskyvorobei2002@mail.ru>
2025-08-08 16:58:09 +0700

    Update lesson_2_task_6.py

--- lesson_2_task_6.py
+++ lesson_2_task_6.py
@@ -1,1 +1,4 @@
+lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
+filtered_list = [x for x in lst if x < 30 and x % 3 == 0]
 
+print("Элементы, которые меньше 30 и делятся на 3:", filtered_list)
