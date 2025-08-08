afe85666b097288a9f5cb17fb1c13ee9de580c97
sky <kalininskyvorobei2002@mail.ru>
2025-08-08 18:45:26 +0700

    Update lesson_2_task_4.py

--- lesson_2_task_4.py
+++ lesson_2_task_4.py
@@ -1,10 +1,10 @@
 def fizz_buzz(n):
   for num in range(1, n + 1):
-    if num % 15 ==0:
+    if num % 15 == 0:
 
       print("FizzBuzz")
 elif num % 3 == 0:
-      print("Fizz")
+      print("fizz")
 elif num % 5 == 0:
       print("Buzz")
 else:
