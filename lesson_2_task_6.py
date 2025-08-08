7e1fdd08ff3efaed63823ab0799259d6fd60e8e2
sky <kalininskyvorobei2002@mail.ru>
2025-08-08 19:11:09 +0700

    Update lesson_2_task_6.py

--- lesson_2_task_6.py
+++ lesson_2_task_6.py
@@ -1,4 +1,6 @@
+def filter_list(lst):
+  return [x for x in lst if x < 30 and x % 3 == 0]
 lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
-filtered_list = [x for x in lst if x < 30 and x % 3 == 0]
-
-print("Элементы, которые меньше 30 и делятся на 3:", filtered_list)
+filtered = filter_list(lst)
+print("Исходный список:", lst)
+print("Отфильтрованный список (числа <30, кратные 3):", filtered)
