5da3addbaab1151f39d88d9befcd57eaab10014c
sky <kalininskyvorobei2002@mail.ru>
2025-10-27 14:55:52 +0700

    Update lesson_2_task_5.py

--- lesson_2_task_5.py
+++ lesson_2_task_5.py
@@ -1,19 +1,21 @@
-def month_to_season(month):
-  if month in [12, 1, 2]:
-    return "Зима"
-  elif month in [3, 4, 5]:
-    return "Весна"
-  elif month in [6, 7, 8]:
-    return "Лето"
-  elif month in [9, 10, 11]:
-    return "Осень"
-  else:
-    raise ValueError("Некорректный номер месяца. Введите число от 1 до 12.")
- if __name__ == "__main__":
-   test_cases = [1, 3, 6, 9, 12, 15]
+# Напишите функцию month_to_season(), которая принимает один аргумент — номер
+# месяца — и возвращает название сезона, к которому относится этот месяц.
+def month_to_season(number):
+    if not 1 <= number <= 12:
+        raise ValueError('Месяц должен быть от 1 до 12')
+    if 1 <= number <= 2 or number == 12:
+        return 'Зима'
+    elif 3 <= number <= 5:
+        return 'Весна'
+    elif 6 <= number <= 8:
+        return 'Лето'
+    else:
+        return 'Осень'
 
-   for month in test_cases:
-       try:
-           print(f"Месяц {month}: {month_to_season(month)}")
-except ValueError as e:
-   print(f"Месяц {month}: {e}")
+
+try:
+    number = int(input('Введите месяц от 1 до 12: '))
+    season = month_to_season(number)
+    print(season)
+except ValueError:
+    print("Ошибка: нужно ввести целое число от 1 до 12")
