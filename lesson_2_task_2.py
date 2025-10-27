ad5b2fe2a52214dd6c0f5d7f461eeb00eac8ee60
sky <kalininskyvorobei2002@mail.ru>
2025-10-27 14:55:07 +0700

    Update lesson_2_task_2.py

--- lesson_2_task_2.py
+++ lesson_2_task_2.py
@@ -1,7 +1,9 @@
+# Создайте функцию is_year_leap, принимающую 1 аргумент — год (число)
+#  — и возвращающую True, если год високосный, и False — если иначе.
 def is_year_leap(year):
-  return year % 4 == 0
-  year_to_check = [2024, 2023, 2000, 1900, 2020]
-  for year in years_to_check:
-    result = is_year_leap(year)
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
-print(f"год {year}: {result}")
+
+year = 2025
+result = is_year_leap(year)
+print(f'{year}: {result}')
